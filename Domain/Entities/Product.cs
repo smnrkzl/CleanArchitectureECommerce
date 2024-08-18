8f774c080636c5401b1c7db1305520b31ace13a9
smnrkzl <kizilsemanur@gmail.com>
2024-08-18 14:29:38 +0300

    Add project files.

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Product:Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
